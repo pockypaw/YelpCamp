730295eef1783e4ccb237ff81a5c063f1ca1ae75
Ardian <ardridhz@gmail.com>
2025-01-14 11:34:45 +0700

    created redirect after login

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -28,6 +28,7 @@ const verifyPassword = (req, res, next) => {
 };
 
 const isLoggedIn = (req, res, next) => {
+  req.session.returnTo = req.originalUrl;
   if (!req.isAuthenticated()) {
     req.flash("error", "You must be signed in first");
     return res.redirect("/login");
--- routes/auth.js
+++ routes/auth.js
@@ -38,7 +38,8 @@ router.post(
   }),
   (req, res) => {
     req.flash("success", "Welcome back!");
-    res.redirect("/campgrounds");
+    const redirectUrl = req.session.returnTo || "/campgrounds";
+    res.redirect(redirectUrl);
   }
 );
 
